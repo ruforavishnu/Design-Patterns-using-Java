3951731150414fe817fe14c9a2f1d62e5b9fb124
unknown <vishnuajith310@gmail.com>
2022-04-17 17:03:44 +0530

    Sunday evening commit

---
+++ Service Locator Pattern/Service1.java
@@ -0,0 +1,13 @@
+public class Service1 implements Service
+{
+	public void execute()
+	{
+		System.out.println("Executing Service1");
+	}
+
+	@Override
+	public String getName()
+	{
+		return "Service1";
+	}
+}
